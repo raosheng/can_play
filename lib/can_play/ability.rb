93dea8fbd2f628fc451eb0b12cb48be2d6ac9307
刘志明 <liuzhiming@liuzhimingdeMac-mini.local>
2015-08-19 10:17:36 +0800

    修改user的has_role?方法为role?

--- lib/can_play/ability.rb
+++ lib/can_play/ability.rb
@@ -5,7 +5,7 @@ class Ability
   def initialize(user)
     self.user = user||CanPlay::Config.user_class_name.constantize.new
     CanPlay::Config.role_class_name.constantize.all.each do |role|
-      next unless user.has_role?(role.name)
+      next unless user.role?(role.name)
       role.send(CanPlay::Config.role_resources_middle_class_name.underscore.pluralize).each do |role_resource|
         resource = CanPlay::Config.resource_class_name.constantize.find_by_name(role_resource.resource_name)
         next unless resource
